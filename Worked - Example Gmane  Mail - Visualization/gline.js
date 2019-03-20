4591714647ab88fb15aea83e1b20c242e1808694
Ridi <32238362+ridilnr@users.noreply.github.com>
2019-03-20 15:06:02 +0200

    Add files via upload

---
+++ Worked - Example Gmane  Mail - Visualization/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','cam.ac.uk','mac.com','indiana.edu','berkeley.edu','uct.ac.za','unicon.net','ucdavis.edu','gmail.com','yale.edu'],
+['2005',57,7,12,12,12,14,6,11,10,5],
+['2006',734,324,304,287,247,226,211,185,161,132]
+];
