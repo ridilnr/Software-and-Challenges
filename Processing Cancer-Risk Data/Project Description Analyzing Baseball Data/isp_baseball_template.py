1e269b6b178e4fd45d55af61e3800d7adfe765c6
Ridi <32238362+ridilnr@users.noreply.github.com>
2019-03-20 15:30:01 +0200

    Add files via upload

---
+++ Processing Cancer-Risk Data/Project Description Analyzing Baseball Data/isp_baseball_template.py
@@ -0,0 +1,336 @@
+"""
+Project for Week 4 of "Python Data Analysis".
+Processing CSV files with baseball stastics.
+
+Be sure to read the project description page for further information
+about the expected behavior of the program.
+"""
+
+import csv
+
+##
+## Provided code from Week 3 Project
+##
+
+def read_csv_as_list_dict(filename, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a list of dictionaries where each item in the list
+      corresponds to a row in the CSV file.  The dictionaries in the
+      list map the field names to the field values for that row.
+    """
+    table = []
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=separator, quotechar=quote)
+        for row in csvreader:
+            table.append(row)
+    return table
+
+
+def read_csv_as_nested_dict(filename, keyfield, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      keyfield  - field to use as key for rows
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a dictionary of dictionaries where the outer dictionary
+      maps the value in the key_field to the corresponding row in the
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
+    """
+    table = {}
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=separator, quotechar=quote)
+        for row in csvreader:
+            rowid = row[keyfield]
+            table[rowid] = row
+    return table
+
+##
+## Provided formulas for common batting statistics
+##
+
+# Typical cutoff used for official statistics
+MINIMUM_AB = 500
+
+def batting_average(info, batting_stats):
+    """
+    Inputs:
+      batting_stats - dictionary of batting statistics (values are strings)
+    Output:
+      Returns the batting average as a float
+    """
+    hits = float(batting_stats[info["hits"]])
+    at_bats = float(batting_stats[info["atbats"]])
+    if at_bats >= MINIMUM_AB:
+        return hits / at_bats
+    else:
+        return 0
+
+def onbase_percentage(info, batting_stats):
+    """
+    Inputs:
+      batting_stats - dictionary of batting statistics (values are strings)
+    Output:
+      Returns the on-base percentage as a float
+    """
+    hits = float(batting_stats[info["hits"]])
+    at_bats = float(batting_stats[info["atbats"]])
+    walks = float(batting_stats[info["walks"]])
+    if at_bats >= MINIMUM_AB:
+        return (hits + walks) / (at_bats + walks)
+    else:
+        return 0
+
+def slugging_percentage(info, batting_stats):
+    """
+    Inputs:
+      batting_stats - dictionary of batting statistics (values are strings)
+    Output:
+      Returns the slugging percentage as a float
+    """
+    hits = float(batting_stats[info["hits"]])
+    doubles = float(batting_stats[info["doubles"]])
+    triples = float(batting_stats[info["triples"]])
+    home_runs = float(batting_stats[info["homeruns"]])
+    singles = hits - doubles - triples - home_runs
+    at_bats = float(batting_stats[info["atbats"]])
+    if at_bats >= MINIMUM_AB:
+        return (singles + 2 * doubles + 3 * triples + 4 * home_runs) / at_bats
+    else:
+        return 0
+
+
+##
+## Part 1: Functions to compute top batting statistics by year
+##
+
+def filter_by_year(statistics, year, yearid):
+    """
+    Inputs:
+      statistics - List of batting statistics dictionaries
+      year       - Year to filter by
+      yearid     - Year ID field in statistics
+    Outputs:
+      Returns a list of batting statistics dictionaries that
+      are from the input year.
+    """
+    new_list = list()
+    for num in range(len(statistics)):
+        if (yearid, str(year)) in statistics[num].items():
+            new_list.append(statistics[num])
+    return new_list
+
+
+def top_player_ids(info, statistics, formula, numplayers):
+    """
+    Inputs:
+      info       - Baseball data information dictionary
+      statistics - List of batting statistics dictionaries
+      formula    - function that takes an info dictionary and a
+                   batting statistics dictionary as input and
+                   computes a compound statistic
+      numplayers - Number of top players to return
+    Outputs:
+      Returns a list of tuples, player ID and compound statistic
+      computed by formula, of the top numplayers players sorted in
+      decreasing order of the computed statistic.
+    """
+    new_list = list()
+    for num in range(len(statistics)):
+        new_list.append((statistics[num][info['playerid']], formula(info, statistics[num])))
+    new_list.sort(reverse=True, key=lambda pair: pair[1])
+    top_players = list()
+    for num in range(numplayers):
+        top_players.append(new_list[num])
+    return top_players
+
+def lookup_player_names(info, top_ids_and_stats):
+    """
+    Inputs:
+      info              - Baseball data information dictionary
+      top_ids_and_stats - list of tuples containing player IDs and
+                          computed statistics
+    Outputs:
+      List of strings of the form "x.xxx --- FirstName LastName",
+      where "x.xxx" is a string conversion of the float stat in
+      the input and "FirstName LastName" is the name of the player
+      corresponding to the player ID in the input.
+    """
+    filename = info['masterfile']
+    key_field = info['playerid']
+    separator = info['separator']
+    quote = info['quote']
+    first_name = info['firstname']
+    last_name = info['lastname']
+    my_list = read_csv_as_nested_dict(filename, key_field, separator, quote)
+    player_names = list()
+    for idx in range(len(top_ids_and_stats)):
+        player_id = top_ids_and_stats[idx][0]
+        batting = top_ids_and_stats[idx][1]
+        if player_id in my_list:
+            names = (my_list[player_id][first_name], my_list[player_id][last_name])
+            player_names.append("{0:0.3f} --- {1} {2}".format(batting, names[0], names[1]))
+    return player_names
+
+def compute_top_stats_year(info, formula, numplayers, year):
+    """
+    Inputs:
+      info        - Baseball data information dictionary
+      formula     - function that takes an info dictionary and a
+                    batting statistics dictionary as input and
+                    computes a compound statistic
+      numplayers  - Number of top players to return
+      year        - Year to filter by
+    Outputs:
+      Returns a list of strings for the top numplayers in the given year
+      according to the given formula.
+    """
+    my_list = read_csv_as_list_dict(info['battingfile'], info['separator'], info['quote'])
+    statistics = list()
+    for idx in range(len(my_list)):
+        my_dict = dict(my_list[idx])
+        if (info['yearid'], str(year)) in my_dict.items():
+            statistics.append(my_dict)
+    top_players = top_player_ids(info, statistics, formula, numplayers)
+    return lookup_player_names(info, top_players)
+
+
+##
+## Part 2: Functions to compute top batting statistics by career
+##
+
+def aggregate_by_player_id(statistics, playerid, fields):
+    """
+    Inputs:
+      statistics - List of batting statistics dictionaries
+      playerid   - Player ID field name
+      fields     - List of fields to aggregate
+    Output:
+      Returns a nested dictionary whose keys are player IDs and whose values
+      are dictionaries of aggregated stats.  Only the fields from the fields
+      input will be aggregated in the aggregated stats dictionaries.
+    """
+    dict_player = dict()
+    player_id_list = list()
+    for row in range(len(statistics)):
+        key = statistics[row][playerid]
+        list_players = dict()
+        summ = 0
+        if key not in player_id_list:
+            player_id_list.append(key)
+            for idx2 in range(len(fields)):
+                str_stat = fields[idx2]
+                list_players[playerid] = key
+                str_stat_int = int(statistics[row][str_stat])
+                list_players[str_stat] = summ + str_stat_int
+            dict_player[key] = dict(list_players)
+        else:
+            for idx2 in range(len(fields)):
+                str_stat = fields[idx2]
+                str_stat_int = int(statistics[row][str_stat])
+                dict_player[key][str_stat] = dict_player[key][str_stat] + str_stat_int
+    return dict_player
+
+def compute_top_stats_career(info, formula, numplayers):
+    """
+    Inputs:
+      info        - Baseball data information dictionary
+      formula     - function that takes an info dictionary and a
+                    batting statistics dictionary as input and
+                    computes a compound statistic
+      numplayers  - Number of top players to return
+    """
+    my_list = read_csv_as_list_dict(info['battingfile'], info['separator'], info['quote'])
+    statistics = list()
+    for idx in range(len(my_list)):
+        my_dict = dict(my_list[idx])
+        statistics.append(my_dict)
+    dict_aggregate = aggregate_by_player_id(statistics, info['playerid'], info['battingfields'])
+    new_statistics = list()
+    for value in dict_aggregate.values():
+        new_statistics.append(value)
+    list_top_players = top_player_ids(info, new_statistics, formula, numplayers)
+    return lookup_player_names(info, list_top_players)
+
+##
+## Provided testing code
+##
+
+def test_baseball_statistics():
+    """
+    Simple testing code.
+    """
+
+    #
+    # Dictionary containing information needed to access baseball statistics
+    # This information is all tied to the format and contents of the CSV files
+    #
+    baseballdatainfo = {"masterfile": "Master_2016.csv",   # Name of Master CSV file
+                        "battingfile": "Batting_2016.csv", # Name of Batting CSV file
+                        "separator": ",",                  # Separator character in CSV files
+                        "quote": '"',                      # Quote character in CSV files
+                        "playerid": "playerID",            # Player ID field name
+                        "firstname": "nameFirst",          # First name field name
+                        "lastname": "nameLast",            # Last name field name
+                        "yearid": "yearID",                # Year field name
+                        "atbats": "AB",                    # At bats field name
+                        "hits": "H",                       # Hits field name
+                        "doubles": "2B",                   # Doubles field name
+                        "triples": "3B",                   # Triples field name
+                        "homeruns": "HR",                  # Home runs field name
+                        "walks": "BB",                     # Walks field name
+                        "battingfields": ["AB", "H", "2B", "3B", "HR", "BB"]}
+
+    print("Top 5 batting averages in 1923")
+    top_batting_average_1923 = compute_top_stats_year(baseballdatainfo, batting_average, 5, 1923)
+    for player in top_batting_average_1923:
+        print(player)
+    print("")
+
+    print("Top 10 batting averages in 2010")
+    top_batting_average_2010 = compute_top_stats_year(baseballdatainfo, batting_average, 10, 2010)
+    for player in top_batting_average_2010:
+        print(player)
+    print("")
+
+    print("Top 10 on-base percentage in 2010")
+    top_onbase_2010 = compute_top_stats_year(baseballdatainfo, onbase_percentage, 10, 2010)
+    for player in top_onbase_2010:
+        print(player)
+    print("")
+
+    print("Top 10 slugging percentage in 2010")
+    top_slugging_2010 = compute_top_stats_year(baseballdatainfo, slugging_percentage, 10, 2010)
+    for player in top_slugging_2010:
+        print(player)
+    print("")
+
+    # You can also use lambdas for the formula
+    #  This one computes onbase plus slugging percentage
+    print("Top 10 OPS in 2010")
+    top_ops_2010 = compute_top_stats_year(baseballdatainfo,
+                                          lambda info, stats: (onbase_percentage(info, stats) +
+                                                               slugging_percentage(info, stats)),
+                                          10, 2010)
+    for player in top_ops_2010:
+        print(player)
+    print("")
+
+    print("Top 20 career batting averages")
+    top_batting_average_career = compute_top_stats_career(baseballdatainfo, batting_average, 20)
+    for player in top_batting_average_career:
+        print(player)
+    print("")
+
+
+# Make sure the following call to test_baseball_statistics is
+# commented out when submitting to OwlTest/CourseraTest.
+
+# test_baseball_statistics()
